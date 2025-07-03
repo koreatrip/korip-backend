cc0ece4b3ef634baa5e9140457af9eea1f689e97
hi-rice <hi93rice@gmail.com>
2025-07-04 01:27:21 +0900

    feat: Place 모델 기본 구조 및 Admin 구현
    - Place 모델 기본 필드 추가
    - Place Admin 기본 설정
    - Place 모델 및 Admin 테스트 코드 구현

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,64 @@
 from django.contrib import admin
+from places.models import Place
 
-# Register your models here.
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "content_id",
+        "category_id",
+        "sub_category_id",
+        "region_id",
+        "view_count",
+        "created_at"
+    ]
+
+    list_filter = [
+        "category_id",
+        "sub_category_id",
+        "region_id",
+        "created_at"
+    ]
+
+    search_fields = [
+        "content_id",
+    ]
+
+    readonly_fields = [
+        "id",
+        "created_at",
+        "updated_at",
+        "last_synced_at"
+    ]
+
+    fieldsets = [
+        ("기본 정보", {
+            "fields": [
+                "content_id",
+                "latitude",
+                "longitude",
+                "phone_number",
+                "opening_hours"
+            ]
+        }),
+        ("카테고리 및 지역", {
+            "fields": [
+                "category_id",
+                "sub_category_id",
+                "region_id"
+            ]
+        }),
+        ("통계", {
+            "fields": [
+                "view_count"
+            ]
+        }),
+        ("시스템", {
+            "fields": [
+                "created_at",
+                "updated_at",
+                "last_synced_at"
+            ]
+        })
+    ]
