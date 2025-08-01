70f3cc171b34df8c2ea435b46f1465ca36a2e01f
SwanyCastle <seunghwan712@gmail.com>
2025-08-01 15:22:47 +0900

    Fix: 인증번호 유효기간 수정

--- users/views/email_verification.py
+++ users/views/email_verification.py
@@ -51,7 +51,7 @@ class SendVerificationCodeAPIView(BaseAPIView):
 
             if verification_code is None:
                 raise EmailError(ErrorCode.EMAIL_NOT_CERTIFIED)
-            self.redis_helper.set_with_expiry(f"email_verification:{email}", verification_code, 600)
+            self.redis_helper.set_with_expiry(f"email_verification:{email}", verification_code, 60)
             return Response(status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
