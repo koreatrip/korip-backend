747ac4787b889436ce310a01ffd6722fc614e2c7
hi-rice <hi93rice@gmail.com>
2025-08-19 20:19:29 +0900

    fix: Region API에 feature 필드 추가 및 응답 형식 통일
    - RegionSerializer, SubRegionSerializer에 feature 필드 추가
    - get_feature 메서드 오타 수정 (translation.feature → translation.features)
    - RegionDetailSerializer에 feature 필드 추가
    - API 응답 키를 'regions'로 통일 (기존 region → regions)

--- regions/serializers.py
+++ regions/serializers.py
@@ -4,97 +4,117 @@ from regions.models import Region, RegionTranslation, SubRegion, SubRegionTransl
 
 class RegionSerializer(serializers.ModelSerializer):
 
-    name = serializers.SerializerMethodField()
-    description = serializers.SerializerMethodField()
+   name = serializers.SerializerMethodField()
+   description = serializers.SerializerMethodField()
+   feature = serializers.SerializerMethodField()
 
-    class Meta:
-        model = Region
-        fields = ["id", "name", "description"]
+   class Meta:
+       model = Region
+       fields = ["id", "name", "description", "feature"]
 
-    def get_name(self, obj):
-        language = self.context.get("language", "ko")
+   def get_name(self, obj):
+       language = self.context.get("language", "ko")
 
-        try:
-            translation = obj.translations.filter(lang=language).first()
-            return translation.name if translation else ""
-        except:
-            return ""
+       try:
+           translation = obj.translations.filter(lang=language).first()
+           return translation.name if translation else ""
+       except:
+           return ""
 
-    def get_description(self, obj):
-        language = self.context.get("language", "ko")
+   def get_description(self, obj):
+       language = self.context.get("language", "ko")
 
-        try:
-            translation = obj.translations.filter(lang=language).first()
-            return translation.description if translation else ""
-        except:
-            return ""
+       try:
+           translation = obj.translations.filter(lang=language).first()
+           return translation.description if translation else ""
+       except:
+           return ""
+
+   def get_feature(self, obj):
+       language = self.context.get("language", "ko")
+
+       try:
+           translation = obj.translations.filter(lang=language).first()
+           return translation.features if translation else ""
+       except:
+           return ""
 
 
 class SubRegionSerializer(serializers.ModelSerializer):
 
-    name = serializers.SerializerMethodField()
-    description = serializers.SerializerMethodField()
-    feature = serializers.SerializerMethodField()
+   name = serializers.SerializerMethodField()
+   description = serializers.SerializerMethodField()
+   feature = serializers.SerializerMethodField()
 
-    class Meta:
-        model = SubRegion
-        fields = [
-            "id", "name", "description", "feature",
-            "favorite_count", "latitude", "longitude"
-        ]
+   class Meta:
+       model = SubRegion
+       fields = [
+           "id", "name", "description", "feature",
+           "favorite_count", "latitude", "longitude"
+       ]
 
-    def get_name(self, obj):
-        language = self.context.get("language", "ko")
+   def get_name(self, obj):
+       language = self.context.get("language", "ko")
 
-        try:
-            translation = obj.translations.filter(lang=language).first()
-            return translation.name if translation else ""
-        except:
-            return ""
+       try:
+           translation = obj.translations.filter(lang=language).first()
+           return translation.name if translation else ""
+       except:
+           return ""
 
-    def get_description(self, obj):
-        language = self.context.get("language", "ko")
+   def get_description(self, obj):
+       language = self.context.get("language", "ko")
 
-        try:
-            translation = obj.translations.filter(lang=language).first()
-            return translation.description if translation else ""
-        except:
-            return ""
+       try:
+           translation = obj.translations.filter(lang=language).first()
+           return translation.description if translation else ""
+       except:
+           return ""
 
-    def get_feature(self, obj):
-        language = self.context.get("language", "ko")
+   def get_feature(self, obj):
+       language = self.context.get("language", "ko")
 
-        try:
-            translation = obj.translations.filter(lang=language).first()
-            return translation.feature if translation else ""
-        except:
-            return ""
+       try:
+           translation = obj.translations.filter(lang=language).first()
+           return translation.features if translation else ""
+       except:
+           return ""
 
 
 class RegionDetailSerializer(serializers.ModelSerializer):
 
-    name = serializers.SerializerMethodField()
-    description = serializers.SerializerMethodField()
-    subregions = SubRegionSerializer(many=True, read_only=True)
+   name = serializers.SerializerMethodField()
+   description = serializers.SerializerMethodField()
+   feature = serializers.SerializerMethodField()
+   subregions = SubRegionSerializer(many=True, read_only=True)
 
-    class Meta:
-        model = Region
-        fields = ["id", "name", "description", "subregions"]
+   class Meta:
+       model = Region
+       fields = ["id", "name", "description", "feature", "subregions"]
 
-    def get_name(self, obj):
-        language = self.context.get("language", "ko")
+   def get_name(self, obj):
+       language = self.context.get("language", "ko")
 
-        try:
-            translation = obj.translations.filter(lang=language).first()
-            return translation.name if translation else ""
-        except:
-            return ""
+       try:
+           translation = obj.translations.filter(lang=language).first()
+           return translation.name if translation else ""
+       except:
+           return ""
 
-    def get_description(self, obj):
-        language = self.context.get("language", "ko")
+   def get_description(self, obj):
+       language = self.context.get("language", "ko")
 
-        try:
-            translation = obj.translations.filter(lang=language).first()
-            return translation.description if translation else ""
-        except:
-            return ""
+       try:
+           translation = obj.translations.filter(lang=language).first()
+           return translation.description if translation else ""
+       except:
+           return ""
+
+   def get_feature(self, obj):
+       language = self.context.get("language", "ko")
+
+       try:
+           translation = obj.translations.filter(lang=language).first()
+           return translation.features if translation else ""
+       except:
+           return ""
