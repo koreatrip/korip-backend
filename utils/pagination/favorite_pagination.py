1b272e5cce0a54d91e0d404a4d8eb3a410231c9b
SwanyCastle <seunghwan712@gmail.com>
2025-08-29 15:59:51 +0900

    Fix: 즐겨찾기 페이지네이션 결과값 필드명 동적으로 변경

--- utils/pagination/favorite_pagination.py
+++ utils/pagination/favorite_pagination.py
@@ -6,6 +6,7 @@ class FavoritePagination(PageNumberPagination):
     page_query_param = "page"
     page_size_query_param = 'page_size'
     max_page_size = 100
+    results_field_name = "results"
 
     def get_paginated_response(self, data):
         return Response({
@@ -15,5 +16,5 @@ class FavoritePagination(PageNumberPagination):
             "page_size": self.get_page_size(self.request) or self.page_size,
             'next': self.get_next_link(),
             'previous': self.get_previous_link(),
-            "favorite_places": data,
+            self.results_field_name: data,
         })
