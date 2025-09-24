5a9d076f377fabdf1f05a65726c8834b78dbaafb
SwanyCastle <seunghwan712@gmail.com>
2025-09-24 11:39:22 +0900

    Fix: 구글 소셜로그인 수정
    - 구글 소셜 로그인시 리다이렉트 URI 환경변수로 받도록 수정
    - 구글 소셜 로그인시 처음 로그인한 사용자 표시를 위한 로직 수정

--- utils/oauth/google.py
+++ utils/oauth/google.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 class GoogleOAuth(OAuthProvider):
     client_id = config("GOOGLE_CLIENT_ID")
     client_secret = config("GOOGLE_CLIENT_SECRET")
-    redirect_uri = "http://localhost:8000/oauth2/callback/google"
+    redirect_uri = config("GOOGLE_REDIRECT_URI")
 
     def get_token(self, code: str) -> str:
         url = "https://oauth2.googleapis.com/token"
