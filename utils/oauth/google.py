bf4cf07ddcd296b3107b9090648b5ced692b5085
Ubuntu <ubuntu@ip-172-31-5-131.ap-northeast-2.compute.internal>
2025-09-24 04:00:42 +0000

    [FIX] : 머지 충돌 해결

--- utils/oauth/google.py
+++ utils/oauth/google.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 class GoogleOAuth(OAuthProvider):
     client_id = config("GOOGLE_CLIENT_ID")
     client_secret = config("GOOGLE_CLIENT_SECRET")
-    redirect_uri = config("GOOGLE_REDIRECT_URI")
+    redirect_uri = config("GOOGLE_REDIRECT_URI") 
 
     def get_token(self, code: str) -> str:
         url = "https://oauth2.googleapis.com/token"
@@ -34,3 +34,4 @@ class GoogleOAuth(OAuthProvider):
         user_info = res.json()
         user_info['login_type'] = LoginType.GOOGLE
         return user_info
+
